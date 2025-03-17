a34bc8b6fbd388f7a706e806582570e7e3c88fc2
nr <nr@mex.local>
2025-03-17 10:31:57 -0400

    Add hacky markdown image sizer

--- convert.py
+++ convert.py
@@ -1,4 +1,5 @@
 import os
+import re
 from markdown_it import MarkdownIt
 from mdit_py_plugins.tasklists import tasklists_plugin
 from mdit_py_plugins.footnote import footnote_plugin
@@ -27,8 +28,48 @@ for filename in os.listdir(post_dir):
     if filename.endswith(".md"):
         with open(f"{post_dir}/{filename}", "r", encoding="utf-8") as f:
             md_content = f.read()
-            html_content = md.render(md_content)
-        final_html = template.replace("<!-- Markdown content will be inserted here -->", html_content)
+            cleaned_lines = []
+            img_sizes = {}
+            
+            # You can add html comments to markdown like
+            # <!-- img:width= px -->
+            # This is kind of a hacky workaround tho.
+            # TODO find a better way to convert image sizes from md -> html
+            # ?Is this implemented in the markdown renderer?
+            lines = md_content.split('\n')
+            i = 0
+            while i < len(lines):
+                line = lines[i]
+                if '<!-- img:width=' in line and i < len(lines) - 1:
+                    # Extract width from comment
+                    width = line.split('<!-- img:width=')[1].split('px -->')[0]
+                    # Check if the next line contains an image
+                    next_line = lines[i+1]
+                    if next_line.strip().startswith('!['):
+                        # Extract image path
+                        img_match = re.search(r'!\[.*?\]\((.*?)\)', next_line)
+                        if img_match:
+                            img_path = img_match.group(1)
+                            img_sizes[img_path] = width
+                            # Skip this comment line
+                            i += 1
+                            cleaned_lines.append(next_line)
+                else:
+                    cleaned_lines.append(line)
+                i += 1
+            
+            clean_md_content = '\n'.join(cleaned_lines)
+            clean_md_content = clean_md_content.replace('](/static/', '](../static/')
+            html_content = md.render(clean_md_content)
+            
+            for img_path, width in img_sizes.items():
+                fixed_path = img_path.replace('/static/', '../static/')
+                html_content = html_content.replace(
+                    f'<img src="{fixed_path}" alt=',
+                    f'<img src="{fixed_path}" width="{width}" alt='
+                )
+
+        final_html = template.replace("<!-- MARKDOWN HTML MUTATOR PLACEHOLDER -->", html_content)
         new_filename = filename.replace(".md", ".html")
         with open(f"{output_dir}/{new_filename}", "w", encoding="utf-8") as f:
             f.write(final_html)
--- posts/about-me.md
+++ posts/about-me.md
@@ -1,5 +1,8 @@
 I'm Nick.
 
+<!-- img:width=200px -->
+![A photo of me](/static/me.jpeg)
+
 I write C++ for fun.
 
 In my professional life, I've mostly worked on networking and cryptography code. I am an expert in neither, but I know some things.
